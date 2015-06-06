3287f261566f6e0122c77d767362b6160a575882
Frost Li <frost@radiosocial.io>
2015-06-05 17:48:53 -0700

    remove naming conflict

--- tour/serializers.py
+++ tour/serializers.py
@@ -3,13 +3,13 @@ from tour.models import Tour, Step
 
 
 class TourSerializer(serializers.ModelSerializer):
-    steps = serializers.SerializerMethodField('get_steps_r')
+    steps = serializers.SerializerMethodField('get_first_step')
 
     class Meta:
         model = Tour
         fields = ('name', 'display_name', 'complete_url', 'steps')
 
-    def get_steps_r(self, tour):
+    def get_first_step(self, tour):
         return [
             StepSerializer(child_step, context=self.context).data
             for child_step in tour.load_tour_class().get_steps(0)
@@ -17,20 +17,20 @@ class TourSerializer(serializers.ModelSerializer):
 
 
 class StepSerializer(serializers.ModelSerializer):
-    steps = serializers.SerializerMethodField('get_steps_r')
-    complete = serializers.SerializerMethodField('get_complete_r')
+    steps = serializers.SerializerMethodField('get_first_step')
+    complete = serializers.SerializerMethodField('get_complete_for_user')
 
     class Meta:
         model = Step
         fields = ('name', 'display_name', 'url', 'sort_order', 'steps', 'complete')
 
-    def get_steps_r(self, step):
+    def get_first_step(self, step):
         return [
             StepSerializer(child_step, context=self.context).data
             for child_step in step.load_step_class().get_steps(0)
         ]
 
-    def get_complete_r(self, step):
+    def get_complete_for_user(self, step):
         if 'request' in self.context:
             return step.load_step_class().is_complete(self.context['request'].user)
         return False
